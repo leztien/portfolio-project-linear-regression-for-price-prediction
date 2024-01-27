b761e590559512e3ffe72d72f67e581801e8df56
Ilja Neitzel <i.neitzel@mail.de>
2024-01-27 14:00:04 +0100

    changed utilities, added images

--- src/utilities.py
+++ src/utilities.py
@@ -3,3 +3,18 @@
 """
 TODO
 """
+
+
+from pathlib import Path
+import matplotlib.pyplot as plt
+
+
+def save_fig(name):
+    """Saves the current figure"""
+    path = Path() / "images"
+    if not path.exists():
+        path.mkdir()
+    path /= f"{name}.png"
+    plt.tight_layout()
+    plt.savefig(path, format='png', dpi=300)
+    return str(path)
