203476a3f269c04d9a0d2065d4aeef7e32b7ac12
Ilja Neitzel <i.neitzel@mail.de>
2024-02-02 02:28:43 +0100

    -

---
+++ src/__init__.py
@@ -0,0 +1,2 @@
+
+from .get_peaks import get_peaks  #just to show you can
---
+++ src/get_peaks.py
@@ -0,0 +1,66 @@
+
+
+"""
+get_peaks function finds the peaks in a multimodal distribution
+"""
+
+
+import numpy as np
+
+
+def make_data(m, n_peaks=3, standerdized=False):
+    """
+    Makes 1-dimensional multimodal distribution
+    """
+    means = np.arange(n_peaks) + np.random.normal(scale=0.1, size=n_peaks)
+    sigmas = np.random.randint(2, 5, n_peaks) / 10
+    pp = [1/n_peaks] * n_peaks
+    
+    distributions = [np.random.normal(loc=mu, scale=sd, size=round(p*m))
+                     for mu, sd, p in zip(means, sigmas, pp)]
+    
+    a = np.concatenate(distributions)[:m]
+    
+    if standerdized:
+        return (a - a.mean()) / a.std()
+    return ((a - a.min() + 1) * 10).round()  # immitates 'house age' in years
+
+
+def get_peaks(a, n_peaks, return_bins=False):
+    """
+    Finds n_peaks in distribution 'a'
+    """
+    for n_bins in range(int(len(a)**(1/2)), 5, -1):
+        y, x = np.histogram(a, bins=n_bins)
+        d = np.sign(y[1:] - y[:-1])
+        d2 = d[1:] - d[:-1]
+        mask = d2 == -2
+        if sum(mask) <= n_peaks:
+            break
+    
+    offset = (len(x)-len(mask)) // 2
+    mask = [False]*offset + list(mask) + [False]*(len(x)-len(mask)-offset)
+    try:
+        idx = np.nonzero(mask)[0]
+        peaks = (x[idx] + x[idx+1]) / 2
+    except:
+        peaks = x[mask]
+    return (peaks, n_bins) if return_bins else peaks
+
+
+
+# Demo
+if __name__ == '__main__':
+    import matplotlib.pyplot as plt
+    
+    m = round(np.random.choice(np.logspace(2, 5, num=4, base=10)))
+    n_peaks = np.random.randint(1, 10)
+    
+    a = make_data(m, n_peaks=n_peaks)
+    peaks, n_bins = get_peaks(a, n_peaks=n_peaks, return_bins=True)
+    print(f"m = {m}\nn_peaks = {n_peaks}\nn_bins = {n_bins}\npeaks: {str(list(peaks.round(1)))[1:-1]}")
+    
+    plt.hist(a, bins=n_bins)
+    _, ymax = plt.ylim()
+    plt.vlines(peaks, ymin=0, ymax=ymax, color='k', linewidth=0.9)
+    
