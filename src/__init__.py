3021d9a41b5d535add485d5591ce9041a6e83fce
Ilja Neitzel <48551497+leztien@users.noreply.github.com>
2024-02-14 23:21:57 +0100

    Add files via upload

--- src/__init__.py
+++ src/__init__.py
@@ -1,2 +1,4 @@
 
 from .get_peaks import get_peaks  #just to show you can
+from .feature_engineering import ClusterSimilarity
+
--- src/feature_engineering.py
+++ src/feature_engineering.py
@@ -3,6 +3,9 @@
 from sklearn.cluster import KMeans
 from sklearn.metrics.pairwise import rbf_kernel
 from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.preprocessing import StandardScaler, FunctionTransformer
+from sklearn.pipeline import make_pipeline
+from sklearn.impute import SimpleImputer
 
 
 class ClusterSimilarity(BaseEstimator, TransformerMixin):
@@ -20,3 +23,17 @@ class ClusterSimilarity(BaseEstimator, TransformerMixin):
     def get_feature_names_out(self, feature_names_in=None):
         return [f"similarity_cluster_{i+1}" for i in range(self.n_clusters)]
 
+
+def make_ratio(X):
+    return X[:, [0]] / X[:, [1]]
+
+def feature_names_out(transformer=None, feature_names_in=None):
+    return ["ratio"]
+
+def make_ratio_pipline():
+    return make_pipeline(
+        SimpleImputer(strategy='median'),
+        FunctionTransformer(make_ratio,
+                            feature_names_out=feature_names_out),
+        StandardScaler()
+    )
